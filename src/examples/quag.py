89f2f77807c28c4e69e3c8e9b4362f520b7498f8
Tushar Chandra <me@tusharc.dev>
2023-06-05 21:34:15 -0500

    examples: fix construction passing houses directly

--- src/examples/quag.py
+++ src/examples/quag.py
@@ -88,33 +88,33 @@ if __name__ == "__main__":
     puzzle = (
         puzzle.set_constraints()
         # 1. There is one chair between the place setting with Lilies and the one eating Grilled Cheese.
-        .add_clue(one_between(Flower.lilies, Food.grilled_cheese))
+        .add_clue(one_between(Flower.lilies, Food.grilled_cheese, puzzle.houses))
         # 2. There is one chair between Timothy's Mom and the one eating Stew.
-        .add_clue(one_between(Children.timothy, Food.stew))
+        .add_clue(one_between(Children.timothy, Food.stew, puzzle.houses))
         # 3. There are two chairs between the Bella's Mom and Penny's seat on the right.
-        .add_clue(two_between(Children.bella, Mother.penny))
-        .add_clue(right_of(Mother.penny, Children.bella))
+        .add_clue(two_between(Children.bella, Mother.penny, puzzle.houses))
+        .add_clue(right_of(Mother.penny, Children.bella, puzzle.houses))
         # 4. There is one chair between the place setting with Roses and the one eating Spaghetti on the left.
-        .add_clue(one_between(Flower.roses, Food.spaghetti))
-        .add_clue(left_of(Food.spaghetti, Flower.roses))
+        .add_clue(one_between(Flower.roses, Food.spaghetti, puzzle.houses))
+        .add_clue(left_of(Food.spaghetti, Flower.roses, puzzle.houses))
         # 5. There are two chairs between the place setting with Carnations and Samantha's Mom.
-        .add_clue(two_between(Flower.carnations, Children.samantha))
+        .add_clue(two_between(Flower.carnations, Children.samantha, puzzle.houses))
         # 6. There is one chair between Meredith's Mom and Timothy's Mom on the left.
-        .add_clue(one_between(Children.meredith, Children.timothy))
-        .add_clue(left_of(Children.timothy, Children.meredith))
+        .add_clue(one_between(Children.meredith, Children.timothy, puzzle.houses))
+        .add_clue(left_of(Children.timothy, Children.meredith, puzzle.houses))
         # 7. Aniya's place setting has a lovely Carnation bouquet.
-        .add_clue(same_house(Mother.aniya, Flower.carnations))
+        .add_clue(same_house(Mother.aniya, Flower.carnations, puzzle.houses))
         # 8. There are two chairs between the one eating Grilled Cheese and the one eating Spaghetti.
-        .add_clue(two_between(Food.grilled_cheese, Food.spaghetti))
+        .add_clue(two_between(Food.grilled_cheese, Food.spaghetti, puzzle.houses))
         # 9. The person in the first chair (left-most) is eating Pizza.
         .add_clue(found_at(Food.pizza, 1))
         # 10. The Tulips were placed at one of the place settings somewhere to the left of Penny's chair.
-        .add_clue(left_of(Flower.tulips, Mother.penny))
+        .add_clue(left_of(Flower.tulips, Mother.penny, puzzle.houses))
         # 11. There are two chairs between the one eating Spaghetti and Kailyn's seat.
-        .add_clue(two_between(Food.spaghetti, Mother.kailyn))
+        .add_clue(two_between(Food.spaghetti, Mother.kailyn, puzzle.houses))
         # 12. There is one chair between the one eating Pizza and Holly's chair on the right.
-        .add_clue(one_between(Food.pizza, Mother.holly))
-        .add_clue(right_of(Mother.holly, Food.pizza))
+        .add_clue(one_between(Food.pizza, Mother.holly, puzzle.houses))
+        .add_clue(right_of(Mother.holly, Food.pizza, puzzle.houses))
     )
 
     print(puzzle)
